2a4239330ac1b815d1b007e55ea35153275f5752
Mikolaj Grobelny <miki4920@gmail.com>
2021-01-24 21:05:40 +0000

    Simplified code, removed a dependency on reddit api, moved to a dictionary based system for storing files TODO: Add a file storage, support for imgur and proper upload handling

--- UtilityFunctions.py
+++ UtilityFunctions.py
@@ -1,32 +1,22 @@
 import requests
 import os
 import json
-import re
+import time
 
 
-extensions = [".jpg", ".png"]
 minimum_image_size = 50000
 maximum_image_size = 20485760
 root = "Maps/"
-stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself',
-              'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
-              'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these',
-              'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do',
-              'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
-              'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before',
-              'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again',
-              'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each',
-              'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than',
-              'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'map', 'battlemap', 'battle',
-              'oc', 'free', 'ocart', 'comments', 'gridless', 'grid', 'made', 'battlemapoc', 'inspired', 'level',
-              'storey', 'three', 'maps', 'feedback', 'handdrawn', 'part', 'px', 'xpx', 'art', 'info', 'one', 'two',
-              'three', 'roll', 'virtual', 'version', 'multi', 'original', 'mine', 'four', 'five', 'ppi', 'square',
-              'small', 'multi', 'jpg', 'png', 'inktober', 'scale', 'first', 'units', 'grand', 'assets', 'mapvember',
-              'floor', 'great', 'rpg']
 
 
 def request_file(url):
-    return requests.get(url).content
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
+    print(url)
+    url = url.replace("preview", "i", 1)
+    url = url.replace("external-", "")
+    time.sleep(2)
+    return requests.get(url, headers=headers).content
 
 
 def read_file(path):
@@ -35,21 +25,17 @@ def read_file(path):
 
 
 def save_file(path, content):
-    path = root + path
-    if check_file_size(content) and not os.path.exists(path):
+    if check_file_size(content):
         with open(path, "wb") as file:
             file.write(content)
 
 
 def read_json(file):
-    return json.loads(request_file(file))
-
-
-def find_file_extension(path):
-    for extension in extensions:
-        if path.endswith(extension):
-            return extension
-    return False
+    request = request_file(file)
+    try:
+        return json.loads(request)
+    except ValueError:
+        print("error")
 
 
 def change_file_name(path, new_name):
@@ -60,48 +46,7 @@ def check_file_size(file):
     return minimum_image_size < len(file) < maximum_image_size
 
 
-def simplify_name(name):
-    name = name.lower()
-    extension = find_file_extension(name)
-    name = re.sub(r'\W+', '_', name)
 
-    size = re.search("((?<!\d)\d{1,3}(?!\d))x((?<!\d)\d{1,3}(?!\d))", name)
-
-    name = re.sub(r'[^a-zA-Z_]', '_', name)
-    name = filter_words(name)
-    name = re.sub("(_)+", "_", name)
-    name = re.sub("_$|^_", "", name)
-
-    if size:
-        name = name.replace(size.group(0), "_")
-        name += "_" + f"{size.group(0)}"
-
-    name = filter_words(name) + extension
-    return name
-
-
-def simplify_all_names(root_path):
-    _, _, file_names = next(os.walk(root_path))
-    for i in range(0, len(file_names)):
-        file_path = file_names[i]
-        file_content = read_file(file_path)
-        os.remove(root_path + file_path)
-        file_name = simplify_name(file_path)
-        save_file(file_name, file_content)
-
-
-def filter_words(name):
-    image_name_list = []
-    for word in name.split("_"):
-        if len(word) < 3:
-            pass
-        elif word in stop_words:
-            pass
-        elif word in image_name_list:
-            pass
-        else:
-            image_name_list.append(word)
-    return "_".join(image_name_list)
 
 
 
--- WebScrapper.py
+++ WebScrapper.py
@@ -1,58 +1,121 @@
-import os
-import praw
-from ClientSecrets import token
 from UtilityFunctions import *
-from MapTagger import MapTagger
-from MapUploader import MapUploader
+import os
+import re
 
 
-token()
-api_url = "http://api.pushshift.io/reddit/search/submission/"
 subreddit = "battlemaps"
-score = 10
+minimum_submission_score = 100
 starting_timestamp = 0
-root = "Maps/"
-minimum_maps_per_tag = 5
-upload_url = 'http://192.168.0.40/uploadImage'
+download_directory = "Maps/"
+gridded_only = True
+stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself',
+              'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
+              'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these',
+              'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do',
+              'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
+              'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before',
+              'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again',
+              'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each',
+              'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than',
+              'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'map', 'battlemap', 'battle',
+              'oc', 'free', 'ocart', 'comments', 'gridless', 'grid', 'made', 'battlemapoc', 'inspired', 'level',
+              'storey', 'three', 'maps', 'feedback', 'handdrawn', 'part', 'px', 'xpx', 'art', 'info', 'one', 'two',
+              'three', 'roll', 'virtual', 'version', 'multi', 'original', 'mine', 'four', 'five', 'ppi', 'square',
+              'small', 'multi', 'jpg', 'png', 'inktober', 'scale', 'first', 'units', 'grand', 'assets', 'mapvember',
+              'floor', 'great', 'rpg', 'battlemaps']
 
 
-class WebScrapper(object):
-    def __init__(self, url):
-        self.reddit = praw.Reddit(client_id=os.environ["CLIENT_ID"],
-                                  client_secret=os.environ["SECRET_KEY"],
-                                  user_agent="WebScrapper",
-                                  username="PythonScrapper",
-                                  password=os.environ["REDDIT_PASSWORD"] )
-        self.reddit.read_only = True
-        self.api_url = url
-        self.start_scrapping(starting_timestamp)
-
-    def start_scrapping(self, timestamp):
-        while True:
-            url = self.api_url + f"?subreddit={subreddit}&score=>{score}&after={timestamp}&sort=asc&size=500"
-            json_api = read_json(url)["data"]
-            for submission in json_api:
-                submission = self.reddit.submission(url=submission["full_link"])
-                self.save_submission(submission)
-            if len(json_api) < 1:
-                return
-            timestamp = int(json_api[-1]["created_utc"])
-            print(timestamp)
+def check_submission(submission):
+    if "preview" not in submission.keys():
+        return False
+    if gridded_only and not get_size(submission["title"]):
+        return False
+    return True
 
 
-    @staticmethod
-    def save_submission(submission):
-        file_extension = find_file_extension(submission.url)
-        if file_extension and re.search("((?<!\d)\d{1,3}(?!\d))x((?<!\d)\d{1,3}(?!\d))", submission.title):
-            image_name = simplify_name(submission.title + file_extension)
-            print(image_name)
-            image = request_file(submission.url)
-            save_file(image_name, image)
+def filter_words(name):
+    name_list = []
+    for word in name.split("_"):
+        if len(word) < 3:
+            pass
+        elif word in stop_words:
+            pass
+        elif word in name_list:
+            pass
+        else:
+            name_list.append(word)
+    return "_".join(name_list)
 
 
-# scrapper = WebScrapper(api_url)
-# simplify_all_names(root)
-map_tagger = MapTagger(root, minimum_maps_per_tag)
-metadata_dictionary = map_tagger.get_metadata_dictionary(root)
-uploader = MapUploader(upload_url, root)
-uploader.upload_dictionary(metadata_dictionary)
+def simplify_name(name):
+    name = name.lower()
+
+    name = re.sub(r'[^a-zA-Z_]', '_', name)
+    name = filter_words(name)
+    name = re.sub("(_)+", "_", name)
+    name = re.sub("_$|^_", "", name)
+    return name
+
+
+def get_url(submission):
+    submission = submission["preview"]["images"][0]["source"]["url"]
+    url = submission.split("?")[0]
+    return url
+
+
+def get_extension(url):
+    return url[-3:]
+
+
+def get_path(name, extension):
+    path = download_directory + name + "." + extension
+    count = 2
+    while os.path.exists(path):
+        path = download_directory + name + f"_{count}" + "." + extension
+        count += 1
+    return download_directory + name + "." + extension
+
+
+def get_size(image_name):
+    size = re.search("((?<!\d)\d{1,3}(?!\d))x((?<!\d)\d{1,3}(?!\d))", image_name)
+    return size.group(0) if size else False
+
+
+def create_submission_dictionary(submission):
+    name = simplify_name(submission["title"])
+    url = get_url(submission)
+    extension = get_extension(url)
+    path = get_path(name, extension)
+    width, height = None, None
+    if get_size(submission["title"]):
+        width, height = list(map(int, get_size(submission["title"]).split("x")))
+    submission_dictionary = {"name": name,
+                             "url": url,
+                             "extension": extension,
+                             "path": path,
+                             "width": width,
+                             "height": height,
+                             "tags": ()
+                             }
+    return submission_dictionary
+
+
+def start_scrapping():
+    timestamp = starting_timestamp
+    submission_list = []
+    while True:
+        url = "http://api.pushshift.io/reddit/search/submission/" + f"?subreddit={subreddit}&score=>{minimum_submission_score}&after={timestamp}&sort=asc&size=500"
+        json_api = list(read_json(url).values())[0]
+        for submission in json_api:
+            if check_submission(submission):
+                submission_dictionary = create_submission_dictionary(submission)
+                file = request_file(submission_dictionary["url"])
+                print(submission_dictionary["width"], submission_dictionary["height"])
+                save_file(submission_dictionary["path"], file)
+                submission_list.append(submission_dictionary)
+        if len(json_api) < 1:
+            break
+        timestamp = int(json_api[-1]["created_utc"])
+
+
+start_scrapping()
