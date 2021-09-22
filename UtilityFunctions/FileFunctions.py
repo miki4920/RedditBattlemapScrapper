b4fc0c3e9d793bdba193e3e181587a8297524cb1
Nicolas Grobelny <miki4920@gmail.com>
2021-09-22 20:27:03 +0100

    Removed automated dictionary creation, added a Try and Except to return an empty list object when file is not found

--- UtilityFunctions/FileFunctions.py
+++ UtilityFunctions/FileFunctions.py
@@ -1,21 +1,17 @@
 import json
-import os
-from pathlib import Path
 
 
 def write_file(path, content):
-    directory = os.path.dirname(path)
-    Path(directory).mkdir(parents=True, exist_ok=True)
     with open(path, "wb") as file:
         file.write(content)
 
 
 def read_json(path):
-    if not os.path.exists(path):
-        with open(path, 'w') as file:
-            json.dump([], file, indent=4)
-    with open(path, 'rb') as file:
-        json_object = file.read()
+    try:
+        with open(path, 'r') as file:
+            json_object = file.read()
+    except FileNotFoundError:
+        json_object = json.dumps([])
     return json.loads(json_object)
 
 
