79b4a89a799984a484e824e3be18ba73f2ed45c7
Nicolas Grobelny <miki4920@gmail.com>
2021-09-22 20:29:55 +0100

    Added submission checking functions. Added automatic timestamp based on the most recent saved submission.

--- WebScrapper.py
+++ WebScrapper.py
@@ -1,48 +1,76 @@
-from ConfigFiles.Config import Config
-from UtilityFunctions.DictionaryFunctions import create_dictionary
-from UtilityFunctions.HashFunctions import hash_image
+import re
+
+from Config import CONFIG
 from MapTagger import MapTagger
-from MapUploader import MapUploader
+from UtilityFunctions.DictionaryFunctions import DictionaryMaker
+from UtilityFunctions.HashFunctions import hash_image
 from UtilityFunctions.NetworkFunctions import get_api_url, request_file
-from UtilityFunctions.SubmissionFunctions import check_title, check_file_size
 from UtilityFunctions.FileFunctions import *
 
 
 class WebScrapper(object):
     def __init__(self):
-        self.submission_list = read_json(Config.dictionary_path)
+        self.submission_list = read_json(CONFIG.dictionary_path)
+        self.subreddits = read_json(CONFIG.subreddits_path)
+        self.black_list_words = set(read_json(CONFIG.black_list_words_path))
+        self.dictionary_maker = DictionaryMaker()
         self.hash_set = set([submission["hash"] for submission in self.submission_list])
 
-    def get_submission(self, submission, subreddit):
-        if check_title(submission, Config.subreddits[subreddit]["grid"]):
-            submission_dictionary = create_dictionary(submission)
+    @staticmethod
+    def image_format(submission):
+        return submission["url"][-3:] not in CONFIG.file_extensions
+
+    @staticmethod
+    def image_origin(submission):
+        return not re.search("i\..*\.it", submission["url"])
+
+    def blacklist(self, submission):
+        image_name = submission["title"].lower()
+        return any([word in image_name for word in self.black_list_words])
+
+    def check_title(self, submission):
+        if self.image_format(submission):
+            return False
+        if self.image_origin(submission):
+            return False
+        if self.blacklist(submission):
+            return False
+        return True
+
+    @staticmethod
+    def check_file_size(submission):
+        return CONFIG.minimum_file_size < len(submission) < CONFIG.maximum_file_size
+
+    def get_submission(self, submission):
+        if self.check_title(submission):
+            submission_dictionary = self.dictionary_maker(submission, submission["created_utc"])
             submission = request_file(submission_dictionary["url"], timeout=1).content
             submission_dictionary["hash"] = hash_image(submission)
-            if check_file_size(submission) and submission_dictionary["hash"] not in self.hash_set:
+            if self.check_file_size(submission) and submission_dictionary["hash"] not in self.hash_set:
                 write_file(submission_dictionary["path"], submission)
                 self.submission_list.append(submission_dictionary)
                 self.hash_set.add(submission_dictionary["hash"])
 
     def scrapper(self):
-        for subreddit in Config.subreddits:
-            timestamp = Config.starting_timestamp
+        for subreddit in self.subreddits:
+            timestamp = self.submission_list[-1]["timestamp"] if len(self.submission_list) > 0 else 0
             while True:
-                url = get_api_url(subreddit, timestamp)
+                url = get_api_url(subreddit, self.subreddits[subreddit], timestamp)
                 try:
                     json_data = request_file(url).json()["data"]
                     if len(json_data) < 1:
                         break
                     for submission in json_data:
-                        self.get_submission(submission, subreddit)
+                        self.get_submission(submission)
                     timestamp = int(json_data[-1]["created_utc"])
                 except json.decoder.JSONDecodeError:
                     timestamp += 1000
-            write_json(Config.dictionary_path, self.submission_list)
+                write_json(CONFIG.dictionary_path, self.submission_list)
 
 
 webscrapper = WebScrapper()
 webscrapper.scrapper()
-map_tagger = MapTagger()
-map_tagger.assign_tags()
-map_uploader = MapUploader()
-map_uploader.upload_dictionary()
+# map_tagger = MapTagger()
+# map_tagger.assign_tags()
+# map_uploader = MapUploader()
+# map_uploader.upload_dictionary()
